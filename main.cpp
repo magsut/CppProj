0a5b429334f17bc501a95b73f55a4cb33997a7a3
magsut <37022572+magsut@users.noreply.github.com>
2021-05-18 16:40:57 +0300

    Add lowercase characters

--- main.cpp
+++ main.cpp
@@ -6,18 +6,16 @@ using namespace std;
 int main() {
     ifstream file;
     string res = "";
-    file.open( "/home/maksim/PycharmProjects/pythonProject1/num.txt");
+    file.open(R"(C:\Users\maksi\Desktop\pythonProject1\num.txt)");
     if(file.is_open()) {
         string s;
-        //getline(file,s);
-        //file.getline(s);
         file >> s;
         cout << s << endl;
         cout << s.size() << endl;
         for(int i = 0; i < s.size(); i++)
         {
             cout << i <<": " <<s[i] -'0' + '0'<< endl;
-            if( (s[i] - '0' + '0') == -48)
+            if( (s[i] - '0' + '0') == -48 || (s[i] - '0' + '0') == -47)
             {
                 continue;
             }
@@ -34,6 +32,18 @@ int main() {
                 case -94: res += 'T';break;
                 case -93: res += 'Y';break;
                 case -91: res += 'X';break;
+                case -80: res += 'A';break;
+                case -78: res += 'B';break;
+                case -75: res += 'E';break;
+                case -70: res += 'K';break;
+                case -68: res += 'M';break;
+                case -67: res += 'H';break;
+                case -66: res += 'O';break;
+                case -128: res += 'P';break;
+                case -127: res += 'C';break;
+                case -126: res += 'T';break;
+                case -125: res += 'Y';break;
+                case -123: res += 'X';break;
                 default: res += s[i] - '0' + '0';
             }
         }
@@ -43,15 +53,15 @@ int main() {
         cout << "File not open";
     file.close();
     ofstream fileout;
-    fileout.open("/home/maksim/PycharmProjects/pythonProject1/num.txt");
+    fileout.open(R"(C:\Users\maksi\Desktop\pythonProject1\num.txt)");
     if(fileout.is_open())
     {
         fileout << res;
     }
     else
-        cout << "File nit open";
+        cout << "File not open";
     fileout.close();
     return 0;
 }
 
-//АВЕКМНОРСТУХ
+//АВЕКМНОРСТУХавекмнорстух
